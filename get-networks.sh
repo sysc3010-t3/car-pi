4675f50015160c36704b48ab914db18d52715c19
Thao-Tran Le-Phuong <thaotran.lp@gmail.com>
2019-11-17 09:49:22 -0500

    Removed '\n' at end

--- get-networks.sh
+++ get-networks.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo iwlist wlan0 scan | grep 'ESSID\|802.11' | sed 's/\(^.*ESSID:\)\|"\|\(\s*Version.\+$\)//g' | sed 's/^.*\//:/g' | sed -r '/\(^\s*$\)\|\(^.*:\)/d' | tr '\n' ',' | sed 's/,$/\n/g' | sed 's/,:/:/g'
+sudo iwlist wlan0 scan | grep 'ESSID\|802.11' | sed 's/\(^.*ESSID:\)\|"\|\(\s*Version.\+$\)//g' | sed 's/^.*\//:/g' | sed -r '/\(^\s*$\)\|\(^.*:\)/d' | tr '\n' ',' | sed 's/,$//g' | sed 's/,:/:/g'
