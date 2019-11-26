b49985479e0d54d4e127486966020ae621c06434
Thao-Tran Le-Phuong <thaotran.lp@gmail.com>
2019-11-26 11:35:31 -0500

    Added Metadata and CloseServer classes

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import json
 from enum import IntEnum
+from os import path
 
 class MsgType(IntEnum):
     ACK = 0
@@ -21,3 +22,47 @@ class Error(IntEnum):
         body = { 'type': MsgType.ERROR, 'error_type': errType,
                 'error_msg': errMsg }
         return json.dumps(body).encode('utf-8')
+
+class Metadata(object):
+    """
+    Represents the metadata of the car. Stores the `user_id` and `car_id` in a
+    file.
+    """
+
+    FILENAME = 'metadata.json'
+
+    def __init__(self):
+        """
+        Load existing metadata from JSON file or create new JSON file with empty
+        metadata.
+        """
+
+        if path.exists(self.FILENAME):
+            with open(self.FILENAME) as f:
+                self.data = json.load(f)
+        else:
+            self.data = {'user_id':'','car_id':''}
+            with open(self.FILENAME, 'w') as f:
+                json.dump(self.data, f)
+
+    def get_user_id(self):
+        return self.data['user_id']
+
+    def set_user_id(self, userID):
+        self.data['user_id'] = userID
+        with open(self.FILENAME, 'w') as f:
+            json.dump(self.data, f)
+
+    def get_car_id(self):
+        return self.data['car_id']
+
+    def set_car_id(self, carID):
+        self.data['car_id'] = carID
+        with open(self.FILENAME, 'w') as f:
+            json.dump(self.data, f)
+
+class CloseServer(Exception):
+    """
+    Exception that is raised to indicate that the server should be closed.
+    """
+    pass
